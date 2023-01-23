87905b736fa4d482c05492490c194f95646f7d3b
run <tinuvazhayil@gmail.com>
2023-01-23 15:55:33 -0500

    added a home page view

--- index.js
+++ index.js
@@ -3,23 +3,26 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 
+app.set('view engine', 'jsx')
+app.engine('jsx', require('express-react-views').createEngine())
+
+
+
 
 app.use('/places', require('./controllers/places'))
 
 
 app.get('/', (req,res) => {        //home page route
-    res.send('Hello There')
+    res.render('Home')
 })
-
-
-
-
 
 
 
 app.get('*', (req, res) => {      //404 route / error page 
     res.status(404).send('<h1>404 Page</h1>')
 })
+
+
 
 app.listen(process.env.PORT)
 

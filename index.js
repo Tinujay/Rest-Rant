2b338568b5bb12d698f5630881c1ad6b638916b8
run <tinuvazhayil@gmail.com>
2023-01-24 17:56:19 -0500

    adding styles to Rest-Rant

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
+//Modules and Globals
 require('dotenv').config();
-
 const express = require('express');
 const app = express();
 
+//Express Settings
+// app.set('views', _dirname + '/views')
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
+app.use(express.static('public'))
 
 
+//Controllers & Routes
 app.use('/places', require('./controllers/places.js'))
-
 
 
 app.get('/', (req,res) => {        //home page route
@@ -22,7 +25,7 @@ app.get('*', (req, res) => {      //404 route / error page
 })
 
 
-
+//Listen for Connections 
 app.listen(process.env.PORT)
 
 

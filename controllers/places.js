34d537606f91bb805a9610a34e8e60a24e75101f
run <tinuvazhayil@gmail.com>
2023-02-06 21:40:35 -0500

    added custom error message

--- controllers/places.js
+++ controllers/places.js
@@ -31,15 +31,26 @@ router.post('/', (req, res) => {
     req.body.pic = 'http://placekitten.com/400/400'
   }
 
-  db.Place.create(req.body)
-  .then(() => {
-      res.redirect('/places')
-  })
-  .catch(err => {
-      console.log('err', err)
-      res.render('error404')
-  })
+    db.Place.create(req.body)
+    .then(() => {
+        res.redirect('/places')
+    })
+    .catch(err => {
+      if (err && err.name == 'ValidationError') {
+        let message = 'Validation Error: '
+        for (var field in err.errors) {
+            message += `${field} was ${err.errors[field].value}. `
+            message += `${err.errors[field].message}`
+        }
+        console.log('Validation error message', message)
+        res.render('places/new', { message })
+      }
+        else {
+        res.render('error404')
+      }
+    })
 })
+
 
 
 
